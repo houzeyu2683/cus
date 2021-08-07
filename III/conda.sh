60d5d2a8037c4657cfe8541275881766296e3ab4
Your Name <you@example.com>
2021-08-08 01:12:36 +0800

    <feature>update version

--- III/conda.sh
+++ III/conda.sh
@@ -1,7 +1,7 @@
 echo '=============================='
 echo install to "~/.conda" manually
 echo '=============================='
-file="Miniconda3-py39_4.9.2-Linux-x86_64.sh"
+file="Miniconda3-py39_4.10.3-Linux-x86_64.sh"
 url="https://repo.anaconda.com/miniconda/"
 link=$url$file
 wget $link
