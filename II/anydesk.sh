69e345fb84c38300a30b2c4fc2893c8bc19638f7
Greg <houzeyu2683@gmail.com>
2022-05-18 00:37:57 +0800

    Update anydesk.sh

--- II/anydesk.sh
+++ II/anydesk.sh
@@ -1,12 +1,11 @@
-#wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-#sudo sh -c 'echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list'
-file="anydesk_6.1.1-1_amd64.deb"
-url="https://download.anydesk.com/linux/"
-link="$url$file"
-wget $link
-sudo dpkg -i $file
-rm $file
-echo done
-#sudo apt update
-#sudo apt install anydesk -y
+
+#file="anydesk_6.1.1-1_amd64.deb"
+#url="https://download.anydesk.com/linux/"
+#link="$url$file"
+#wget $link
+#sudo dpkg -i $file
+#rm $file
 #echo done
+sudo apt update
+sudo apt install anydesk -y
+echo done
