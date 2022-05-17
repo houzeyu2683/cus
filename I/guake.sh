7f2052476a14f5f2f865b37723548cdd30f82233
Greg <houzeyu2683@gmail.com>
2022-05-18 00:34:08 +0800

    Update guake.sh

--- I/guake.sh
+++ I/guake.sh
@@ -2,5 +2,6 @@ sudo apt install guake -y
 guake --restore-preferences=guake.default
 dconf load / < guake.dconf
 sudo rm -rf /usr/share/applications/guake-prefs.desktop
+sudo rm -rf /usr/share/applications/guake.desktop
 guake &
 echo done
