b6a90f4cda263c94cd798330ba951923f661f208
Greg <houzeyu2683@gmail.com>
2022-05-19 13:44:33 +0800

    Update guake.sh

--- kde/I/guake.sh
+++ kde/I/guake.sh
@@ -5,5 +5,7 @@ dconf load / < guake.dconf
 sudo rm -rf /usr/share/applications/guake-prefs.desktop
 sudo rm -rf /usr/share/applications/guake.desktop
 #guake &
+#> /dev/null 2>&1 &
+guake > /dev/null 2>&1 &
 echo done
 
