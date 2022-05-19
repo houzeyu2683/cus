6ee298c5d070c160ae8a5b6d3c37ba7e4bbc8715
Your Name <you@example.com>
2022-05-19 12:46:12 +0800

    kde guake key

--- kde/I/guake.sh
+++ kde/I/guake.sh
@@ -1,6 +1,7 @@
 sudo apt install guake -y
-guake --restore-preferences=guake.default
-cat guake.kshortcutsrc >> ~/.config/kglobalshortcutsrc
+#guake --restore-preferences=guake.default
+#cat guake.kshortcutsrc >> ~/.config/kglobalshortcutsrc
+dconf load / < guake.dconf
 sudo rm -rf /usr/share/applications/guake-prefs.desktop
 sudo rm -rf /usr/share/applications/guake.desktop
 guake &
--- kde/I/language.sh
+++ kde/I/language.sh
@@ -1,3 +1,4 @@
+sudo apt install xdg-user-dirs-gtk -y
 export LANG=en_US
 xdg-user-dirs-gtk-update
 echo done
