ac0ba523ce839b76bfd86f03375547594f95b604
houzeyu2683 <houzeyu2683@gmail.com>
2022-05-21 02:12:36 +0800

    dev kde

---
+++ kde/dev/kazam.sh
@@ -0,0 +1,2 @@
+sudo apt-get install kazam -y
+echo done
---
+++ kde/dev/vscode.sh
@@ -0,0 +1,7 @@
+file="code_1.67.1-1651841865_amd64.deb"
+url="https://az764295.vo.msecnd.net/stable/da15b6fd3ef856477bf6f4fb29ba1b7af717770d/"
+link="$url$file"
+wget $link
+sudo dpkg -i $file
+rm $file
+echo done
