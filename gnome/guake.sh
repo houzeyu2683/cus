8fde1168a087db39bb45ce5beca6f3d0afd2465f
Greg <houzeyu2683@gmail.com>
2022-05-21 17:49:50 +0800

    Update guake.sh

--- gnome/guake.sh
+++ gnome/guake.sh
@@ -2,6 +2,7 @@ sudo add-apt-repository ppa:linuxuprising/guake
 sudo apt update
 sudo apt install guake -y
 # mkdir -p ~/.config/autostart/
-sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
+sudo cp /usr/share/guake/autostart-guake.desktop /etc/xdg/autostart/
+# sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 dconf load / < guake.dconf
 echo done
