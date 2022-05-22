baadd788f61d72d1d738eabe1813259a2cb587a0
Greg <houzeyu2683@gmail.com>
2022-05-23 02:00:44 +0800

    Update general.sh

--- gnome/general.sh
+++ gnome/general.sh
@@ -1,15 +1,19 @@
+
+##  Basic setting.
+sh removal.sh
 sh language.sh
+sh shortcut.sh
+
+##  The application.
 sh breeze.sh
 sh conky.sh
 sh git.sh
 sh guake.sh
-sh removal.sh
-sh shortcut.sh
 sh anydesk.sh
 sh chromium.sh
 sh filezilla.sh
 sh gbox.sh
 sh kazam.sh
 sh vlc.sh
-sh vscode.sh
+sh code.sh
 reboot
