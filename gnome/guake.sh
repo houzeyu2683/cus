85090ddc8b3e4aa0a18bd41114849dfcfba8dcc6
Greg <houzeyu2683@gmail.com>
2022-05-23 02:53:30 +0800

    Update guake.sh

--- gnome/guake.sh
+++ gnome/guake.sh
@@ -1,4 +1,6 @@
 sudo apt install guake -y
 sudo cp /usr/share/guake/autostart-guake.desktop /etc/xdg/autostart/guake.desktop
 dconf load / < guake.dconf
+sudo rm -rf /usr/share/applications/guake-prefs.desktop 
+sudo rm -rf /usr/share/applications/guake.desktop 
 echo done
