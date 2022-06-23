6a8134dade762f203b848261944bb4da2d6991b3
Greg <houzeyu2683@gmail.com>
2022-06-24 01:43:37 +0800

    Create conda.sh

---
+++ conda.sh
@@ -0,0 +1,11 @@
+echo '=============================='
+echo install to "~/.conda" manually
+echo '=============================='
+file="Miniconda3-latest-Linux-x86_64.sh"
+url="https://repo.anaconda.com/miniconda/"
+link=$url$file
+wget $link
+sh $file
+rm $file
+echo '\n'export PATH="$"PATH:"'"/home/$USER/.conda/bin"'" >> ~/.bashrc
+echo done
